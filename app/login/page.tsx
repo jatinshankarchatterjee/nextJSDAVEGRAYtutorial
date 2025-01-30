388e8c9d34b3cfb84584d1d9415582060665c435
jatinshankarchatterjee <jatinshankarchatterjee@gmail.com>
2025-01-31 03:39:42 +0530

    changes commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,9 @@
+
 import {LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Login() {
-return (<>
+return (
+<>
 <div className="flex flex-col h-screen w-screen items-center justify-center">
 <h1 className="text-3xl font-bold">
 Hello @ Dave&apos;s repair shop</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
+"use client";
 import React from "react";
-import Link from "next/link"; 
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { LayoutDashboardIcon } from "lucide-react";
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="flex flex-col h-svh justify-center items-center">
-        <h1 className="text-4xl font-bold ">
-          Hello @ Dave&apos;s repair shop</h1>
-
-        <Link href="/dashboard" className="flex items-center gap-2 mt-20">
-        <span>Proceed to Dashboard &nbsp;</span>
+    <>
+      <div className="flex flex-col h-svh justify-center items-center">
+        <h1 className="text-4xl font-bold ">Hello @ Dave&apos;s repair shop</h1>
+        <Button className="m-10" onClick={() => router.push("/dashboard")}>
           <LayoutDashboardIcon />
-          
-        </Link>
-    </div>
-    
+        </Button>
+      </div>
+    </>
   );
 }
