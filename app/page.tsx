db296a4956087c7aa56195b8d6a7090b602f2881
jatinshankarchatterjee <jatinshankarchatterjee@gmail.com>
2025-01-31 01:13:29 +0530

    mod home

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Headerhome from "@/components/header-home";
 export default function Home() {
   return (
-    <>
-      <Headerhome />
+    <><Headerhome />
+    <div className="flex h-svh justify-center items-center">
+        <h1 className="text-5xl font-bold underline">
+          Hello @ Dave&apos;s repair shop</h1>
+    </div>
     </>
   );
 }
