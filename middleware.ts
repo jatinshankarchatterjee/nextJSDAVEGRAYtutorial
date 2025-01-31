8b3f05e247a58552f12ef4fd8a97c5412d987901
jatinshankarchatterjee <jatinshankarchatterjee@gmail.com>
2025-01-31 06:43:27 +0530

    changes commit

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default withAuth(
   async function middleware(req: NextRequest) {
     return withAuth(req);
   },
-  { isReturnToCurrentPage:true}
+  { isReturnToCurrentPage: true }
 );
 
 export const config = {
@@ -13,14 +13,10 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
-     * - auth
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - images (image files)
      * - favicon.ico (favicon file)
-     * - robots.txt (robots file)
-     * - login
      */
-    "/((?!api|auth|_next/static|_next/image|images|favicon.ico|robots.txt|login).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
